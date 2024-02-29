7360ef8ff9257eb3c693011a4ec9fd00e3e367e8
André Felicio <andrefelicio@sisconsultoria.com.br>
2024-02-29 14:28:54 -0300

    DTO com java Record

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,2 +1,6 @@
-package med.voll.api.medico;public record DadosCadastroMedico() {
+package med.voll.api.medico;
+
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
 }
--- api/src/main/java/med/voll/api/medico/Especialidade.java
+++ api/src/main/java/med/voll/api/medico/Especialidade.java
@@ -1,2 +1,8 @@
-package med.voll.api.medico;public enum Especialidade {
+package med.voll.api.medico;
+
+public enum Especialidade {
+    ORTOPEDIA,
+    CARDIOLOGIA,
+    GINECOLOGIA,
+    DERMATOLOGIA
 }
