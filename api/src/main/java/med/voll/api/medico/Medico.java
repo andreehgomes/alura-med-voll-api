171b3d0c1288ad50f6c0a81ab8429d676d275cf4
André Felicio <andrefelicio@sisconsultoria.com.br>
2024-03-02 23:48:40 -0300

    metodo para listar medicos

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -2,13 +2,13 @@ package med.voll.api.medico;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
+import java.util.Objects;
+
 @Table(name = "medicos")
 @Entity(name = "Medico")
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -26,6 +26,75 @@ public class Medico {
 
     @Embedded
     private Endereco endereco;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public Especialidade getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(Especialidade especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Medico medico = (Medico) o;
+        return Objects.equals(id, medico.id) && Objects.equals(nome, medico.nome) && Objects.equals(email, medico.email) && Objects.equals(telefone, medico.telefone) && Objects.equals(crm, medico.crm) && especialidade == medico.especialidade && Objects.equals(endereco, medico.endereco);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, email, telefone, crm, especialidade, endereco);
+    }
 
     public Medico(DadosCadastroMedico dados) {
         this.nome = dados.nome();
